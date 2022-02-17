4ff3601b35b7b96badfe1daf31bf833bf56e4a38
Christian <chritsianshada@gmail.com>
2022-02-16 23:56:18 -0600

    JSON map points attempt

--- resources_Christian/Static/js/logic.js
+++ resources_Christian/Static/js/logic.js
@@ -1,8 +1,8 @@
 // Add console.log to check to see if our code is working.
-console.log("working");
+//console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([40.7, -94.5], 4.5);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -11,5 +11,20 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
+// Then we add our 'streets' title layer to the map.
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let MonarchData = "https://github.com/ChristianShada/Monarch_Butterflies/blob/main/static_Sue/geoJSON/journey_north/JNorth_Adult_First_Site.json";
+
+// Grabbing our GeoJSON data.
+d3.json(MonarchData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+
+// Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+// Loop through the cities array and create on marker for each city.
